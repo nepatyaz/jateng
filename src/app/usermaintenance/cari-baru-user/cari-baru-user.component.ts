18bbba4625bccce5dc3e6f586fcb8a372d20c3a6
nepatyaz <nepatyaz@gmail.com>
2019-01-06 06:54:11 +0700

    add cari baru user

---
+++ src/app/usermaintenance/cari-baru-user/cari-baru-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cari-baru-user',
+  templateUrl: './cari-baru-user.component.html',
+  styleUrls: ['./cari-baru-user.component.css']
+})
+export class CariBaruUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
