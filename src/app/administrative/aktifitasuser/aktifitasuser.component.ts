66a153e0e4bebdecf4a9e474de7d9bed683993a2
nepatyaz <nepatyaz@gmail.com>
2019-01-06 00:58:49 +0700

    add aktifitas user

---
+++ src/app/administrative/aktifitasuser/aktifitasuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aktifitasuser',
+  templateUrl: './aktifitasuser.component.html',
+  styleUrls: ['./aktifitasuser.component.css']
+})
+export class AktifitasuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
