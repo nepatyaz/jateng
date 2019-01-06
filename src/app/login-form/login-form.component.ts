f6482fa156b2b301662bd1ac84de6d79caa8786e
nepatyaz <nepatyaz@gmail.com>
2019-01-06 12:57:44 +0700

    change form

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from 'angular-bootstrap-md';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { Observable } from 'rxjs';
+
+
 
 
 @Component({
@@ -12,37 +14,46 @@ import { Observable } from 'rxjs';
 export class LoginFormComponent implements OnInit {
   users$: Object
   results = ''
-  constructor(private router : Router, private userData: AuthService) { }
+  pesan
+
+  @ViewChild('basicModal') basicModal: ModalDirective;
+
+  constructor(private router: Router, private userData: AuthService) { }
 
   ngOnInit() {
-  //ini file projek yang lagi dikerjakan
+    //ini file projek yang lagi dikerjakan
+   
   }
 
-  
-  loginUser(event){
+
+  loginUser(event) {
     event.preventDefault();
     //console.log(event);
     const target = event.target
     const username = target.querySelector('#username').value
     const password = target.querySelector('#password').value
-    console.log(username + " - "+ password)
+    console.log(username + " - " + password)
     this.userData.getUserDetail(username, password)
-    .subscribe(data =>{
-      if (data.status) {
-        //redirect
-        console.log(data.status)
-        if (data.status === 'OK') {
+      .subscribe(data => {
+        if (data.status) {
+          //redirect
           console.log(data.status)
-          this.router.navigate(['/dashboard'])
-          //this.otentifikasi.setLoggedIn(true)
-        }else {
-          window.alert(data.message)
+          if (data.status === 'OK') {
+            console.log(data.status)
+            this.router.navigate(['/dashboard'])
+          
+            //this.otentifikasi.setLoggedIn(true)
+          } else {
+            this.pesan = data.message
+            //window.alert(data.message)
+          }
+        } else {
+          //window.alert(data.message)
+          this.pesan = data.message
+            
         }
-      } else {
-        window.alert(data.message)
-      }
-    })
-    
+      })
+
   }
 
 }
