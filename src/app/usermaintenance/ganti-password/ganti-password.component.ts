8004bcc5e1ad82d1b98569ad7d71c982c2fcc359
nepatyaz <nepatyaz@gmail.com>
2019-01-06 07:01:08 +0700

    add ganti password

---
+++ src/app/usermaintenance/ganti-password/ganti-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ganti-password',
+  templateUrl: './ganti-password.component.html',
+  styleUrls: ['./ganti-password.component.css']
+})
+export class GantiPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
