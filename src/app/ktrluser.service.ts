591d4848658d062038b5135bc1b534f7d689892b
Budisan6501 <budisan6501@gmail.com>
2019-01-06 17:06:46 +0700

    edit

---
+++ src/app/ktrluser.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KtrluserService {
+
+  constructor(private http: HttpClient) {
+  }
+  getData() {
+    //return this.http.get('https://my-json-server.typicode.com/nepatyaz/rest/status')
+    return this.http.get('/status/')
+  }
+}
