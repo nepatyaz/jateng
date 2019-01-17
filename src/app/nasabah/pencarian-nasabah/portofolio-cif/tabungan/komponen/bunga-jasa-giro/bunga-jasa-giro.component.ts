3d282c4c2f7120eae2c90fbd5e6aa3977d54d342
Nepatyaz Dwi <nepatyaz@gmail.com>
2019-01-17 10:28:22 +0700

    update

--- src/app/nasabah/pencarian-nasabah/portofolio-cif/tabungan/komponen/bunga-jasa-giro/bunga-jasa-giro.component.ts
+++ src/app/nasabah/pencarian-nasabah/portofolio-cif/tabungan/komponen/bunga-jasa-giro/bunga-jasa-giro.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-bunga-jasa-giro',
   templateUrl: './bunga-jasa-giro.component.html',
   styles: []
 })
 export class BungaJasaGiroComponent implements OnInit {
-
+  page = 4;
+  tanggal = Date.now();
   constructor() { }
 
   ngOnInit() {
