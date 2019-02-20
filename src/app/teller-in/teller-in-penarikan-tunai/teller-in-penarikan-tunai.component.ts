5d6722988bba4c03768865cc09557e4e35d7c20d
nepatyaz <nepatyaz@gmail.com>
2019-02-20 13:31:16 +0700

    update

--- src/app/teller-in/teller-in-penarikan-tunai/teller-in-penarikan-tunai.component.ts
+++ src/app/teller-in/teller-in-penarikan-tunai/teller-in-penarikan-tunai.component.ts
@@ -7,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TellerInPenarikanTunaiComponent implements OnInit {
 
-  constructor() {}
+  constructor() { }
   ngOnInit() {
+  }
+
+  formSubmit(event) {
+    event.preventDefault();
+    //buat variabel untuk menyimpan nilai value dari form login
+    const target = event.target
+    // //buat variabel untuk menyimpan nilai/value dari setiap id komponen form
+    const rekening = target.querySelector('#rekening').value
+    var nilaiTransaksi = target.querySelector('#nilaiTransaksi').value
+    var mystring = nilaiTransaksi.replace(/\./g,'')
+    var string2 = parseFloat(mystring.substring(3));
+    if(string2 > 100000000){
+      console.log("nilainya lebih dari seratus juta");
+    }else {
+      
+    }
+    
   }
 
 }
