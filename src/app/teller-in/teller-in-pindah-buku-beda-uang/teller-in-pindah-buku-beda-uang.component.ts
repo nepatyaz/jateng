47200b3f517a98a99e79112c7be49c20572d3932
nepatyaz <nepatyaz@gmail.com>
2019-02-27 13:19:29 +0700

    update validation

--- src/app/teller-in/teller-in-pindah-buku-beda-uang/teller-in-pindah-buku-beda-uang.component.ts
+++ src/app/teller-in/teller-in-pindah-buku-beda-uang/teller-in-pindah-buku-beda-uang.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-teller-in-pindah-buku-beda-uang',
@@ -7,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TellerInPindahBukuBedaUangComponent implements OnInit {
 
-  constructor() { }
+  validatingForm: FormGroup;
+  constructor(private _elementRef : ElementRef) { }
 
   ngOnInit() {
+    this.validatingForm = new FormGroup(
+      {
+       
+        inputTanggal: new FormControl(null, Validators.required),
+        inputNoRekeningDebit: new FormControl(null, [Validators.required, Validators.min(1000000), Validators.max(999999999999)]),
+        inputNilaiDebit: new FormControl(null, [Validators.required, Validators.min(10000), Validators.max(999999999999)]),
+        inputNoRekKredit: new FormControl(null, [Validators.required, Validators.min(1000000), Validators.max(999999999999)]),
+        inputNilaiKredit: new FormControl(null, [Validators.required, Validators.min(10000), Validators.max(999999999999)]),
+        inputNilaiEkuivalen: new FormControl(null, [Validators.required, Validators.min(10000), Validators.max(999999999999)]),
+ 
+      }
+    );
   }
+
+  get inputTanggal() { return this.validatingForm.get('inputTanggal'); }
+  get inputNoRekeningDebit() { return this.validatingForm.get('inputNoRekeningDebit'); }
+  get inputNilaiDebit() { return this.validatingForm.get('inputNilaiDebit'); }
+  get inputNoRekKredit() { return this.validatingForm.get('inputNoRekKredit'); }
+  get inputNilaiKredit() { return this.validatingForm.get('inputNilaiKredit'); }
+  get inputNilaiEkuivalen() { return this.validatingForm.get('inputNilaiEkuivalen'); }
+
 }
